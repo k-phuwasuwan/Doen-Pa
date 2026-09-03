a609e22ebfe1592a1e6d09b1447e453489242fcf
k-phuwasuwan <kantawit.phu@gmail.com>
2026-09-03 21:37:03 +0700

    style: fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { TopNav } from "@/components/navigation/TopNav";
+import { BottomNav } from "@/components/navigation/BottomNav";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +25,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       lang="th"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col bg-cream">
+        <TopNav />
+        <main className="flex-1 pb-16 md:pb-0">
+          {children}
+        </main>
+        <BottomNav />
+      </body>
     </html>
   );
 }
